7ab1b49a53229d8904a9b49216d15b8a5587f903
princeky <intern@outdoorsforyouth.org>
2019-05-04 22:36:02 -0600

    almost

---
+++ src/app/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+
+@Component({
+    selector: 'cms-header',
+    templateUrl:'./header.component.html'
+})
+export class HeaderComponent{
+
+}
